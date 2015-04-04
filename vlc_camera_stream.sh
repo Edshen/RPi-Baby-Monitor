ea643d92a21f8a8e630d110b9d7c94d96235f358
Ed Shen <edz@EDZ-MacBookPro-Retina.local>
2015-04-04 16:08:34 -0400

    Resolution 1280x960

--- vlc_camera_stream.sh
+++ vlc_camera_stream.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-raspivid -o - -t 0 -n -w 640 -h 480 -fps 24 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264
+raspivid -o - -t 0 -n -w 1280 -h 960 -fps 24 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264
